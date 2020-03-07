d85406b4c74f733921039f9bc9d03c86a983ead4
William Ambrozic <william.ambrozic@gmail.com>
2020-03-07 15:48:39 -0500

    Added power of three

---
+++ Power_of_Three/Power_of_Three.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+        if (n == 0) return false;
+        else if (n == 1) return true;
+        if (n % 3 == 0) return isPowerOfThree(n/3);
+        else return false;
+};
